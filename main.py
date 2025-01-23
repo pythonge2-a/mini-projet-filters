772491d6ad03a12087e280d3f330e271d7362105
maxime.otero <maxime.otero@heig-vd.ch>
2025-01-23 20:09:44 +0100

    tests bessel corrigés

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from filters import Filters
+from filters.snk.bessel import highpass,lowpass
 
 def main():
     # Initialisation de Filters
-    filters = Filters()
+    highpass_filters = lowpass()
         # Appeler le calcul des composants pour Tchebychev passe-bas d'ordre 2
-    result = filters.snk.tchebychev.2.components(1000, 2000, 1e-6, 2e-6)
-    print("Résultat des composants :", result)
+    c_vals = [1e-6,1e-9,1e-6,1e-9]
+    tf,stages = highpass_filters.components(order=4,cutoff_freq=1000,c_vals=c_vals)
+    print("Fonction de transfert combinée :", tf)
+    for i, stage in enumerate(stages):
+     print(f"Stage {i+1}:")
+     print("Fonction de transfert :", stage['tf'])
+     print("Paramètres :", stage['params'])
 
 if __name__ == "__main__":
     main()
